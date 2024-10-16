79ff2332f840936f7046fe27827d14a0de9375e5
Cbaumgart004 <baumgart.c@gmail.com>
2024-10-16 08:46:36 -0600

    initial file structure, dependencies, and server boilerplate code

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const userRoutes = require('./routes/api/userRoutes')
+const thoughtRoutes = require('./routes/api/thoughtRoutes')
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/users', userRoutes)
+app.use('/api/thoughts', thoughtRoutes)
+
+mongoose.connect('mongodb://localhost/social-network-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
